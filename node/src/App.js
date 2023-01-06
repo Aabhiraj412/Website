d703274da1be980e8c7a9e82de75ba6d26217780
Aabhiraj412 <aabhiraj412@gmail.com>
2023-01-06 14:11:27 +0530

    adding index page

---
+++ node/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <>
+  <meta charSet="UTF-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Abhiraj</title>
+  <h1>This Is My Website</h1>
+</>
+
+    </>
+  );
+}
+
+export default App;
